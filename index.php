2518d424d533893c092cc1ad94c605e8fa86dad6
Noah Manneschmidt <noah@manneschmidt.net>
2011-10-01 03:05:38 -0500

    Hey look, it parses the games out

--- index.php
+++ index.php
@@ -4,8 +4,12 @@ function getNumberOfGames($tribunalHTML) {
   $doc = new DOMDocument();
   $doc->loadHTML($tribunalHTML);
   $gamecount = $doc->getElementById('h_gamecount');
-  var_export($gamecount);
+  $newdoc = new DomDocument();
+  $newdoc->appendChild($newdoc->importNode($gamecount, TRUE));
+  $html = trim($newdoc->saveHTML());
+  $tag = $gamecount->nodeName;
+  return (int) preg_replace('@^<' . $tag . '[^>]*>|</' . $tag . '>$@', '', $html);
 }
 
-include('sample_page.php');
-getNumberOfGames($page);
+error_reporting(0);
+echo getNumberOfGames(file_get_contents('sample_page.html'));
