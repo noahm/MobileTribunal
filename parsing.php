b6ac28854ac8f9b0cd05503cf59a1e200c12d488
Noah Manneschmidt <noah@manneschmidt.net>
2011-10-02 15:36:26 -0500

    Use tabs for consistency and all the comments

--- parsing.php
+++ parsing.php
@@ -1,11 +1,24 @@
 <?php
 function getNumberOfGames($tribunalHTML) {
-  $doc = new DOMDocument();
-  $doc->loadHTML($tribunalHTML);
-  $gamecount = $doc->getElementById('h_gamecount');
-  $newdoc = new DomDocument();
-  $newdoc->appendChild($newdoc->importNode($gamecount, TRUE));
-  $html = trim($newdoc->saveHTML());
-  $tag = $gamecount->nodeName;
-  return (int) preg_replace('@^<' . $tag . '[^>]*>|</' . $tag . '>$@', '', $html);
+
+	// grab the element from the html
+	$doc = new DOMDocument();
+	$doc->loadHTML($tribunalHTML);
+	$gamecount = $doc->getElementById('h_gamecount');
+
+	// now, grab the contents of that element
+	// (this seems like a stupid limitation of PHP's DOM parsing library)
+
+	// create a document becase the element itself can't saveHTML
+	$newdoc = new DomDocument();
+	$newdoc->appendChild($newdoc->importNode($gamecount, TRUE));
+	$html = trim($newdoc->saveHTML());
+
+	// get the name of whatever tag happened to have the id "h_gamecount" (I expect it's a DIV)
+	$tag = $gamecount->nodeName;
+
+	// strip out the surrounding html
+	$pattern = '@^<' . $tag . '[^>]*>|</' . $tag . '>$@';
+	return (int) preg_replace($pattern, '', $html);
+
 }
