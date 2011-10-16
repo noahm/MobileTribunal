70dd2712486ec1d8a99f2e23902ada1fc3bd61d3
unknown <Aram@aramlaptop.(none)>
2011-10-16 12:06:32 -0700

    Missed a quote.

--- proxy.php
+++ proxy.php
@@ -94,7 +94,7 @@ function tribGetCaptcha($realm, $ch, $cookies)
 function tribSkipCase($caseno, $realm, $ch, $cookies)
 {
 
-	$url = http://$realm.leagueoflegends.com/cases/skip/$caseno";
+	$url = "http://$realm.leagueoflegends.com/cases/skip/$caseno";
 	$result = getHtmlHeaderandCookies($ch, $url, $cookies);
 	$pattern = "/Location: http:\/\/$realm\.leagueoflegends\.com\/tribunal\/case\/([0-9]*)\/review\r\n/isU";
 	if ( $result == false )
