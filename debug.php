1221277415fab623e84059b1a219110e3edbe987
Noah Manneschmidt <noah@manneschmidt.net>
2011-10-24 01:11:34 -0500

    Update debug page to use new tribGetCase auto-parsing

--- debug.php
+++ debug.php
@@ -23,8 +23,7 @@ $result = tribGetCase($caseno, $realm, $ch, $result["cookies"]);
 if ( $result === false )
 	echo "Case-get failed: " . curl_error($ch);
 else {
-	$caseData = tribParseHTML($result['html']);
-	echo "Get Case Cookies: {$result["cookies"]}<br /><br />Number of games: " . $caseData['numGames'] . "<br />";
+	echo "Get Case Cookies: {$result["cookies"]}<br /><br />Number of games: {$result['numGames']} <br />";
 	//echo "<br />Case html: <br />" . nl2br(htmlspecialchars($result["html"]));
 }
 
