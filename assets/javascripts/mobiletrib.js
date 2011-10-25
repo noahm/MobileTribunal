a092572dd3f7aee78e6cb032a9e2459a597977f9
Noah Manneschmidt <noah@manneschmidt.net>
2011-10-25 15:09:07 -0500

    Correct once missed instance of find/replace (fixes #14)

--- assets/javascripts/mobiletrib.js
+++ assets/javascripts/mobiletrib.js
@@ -75,7 +75,7 @@ function offerQuit(data) {
 	if (data === '0') return alert('Your submission was not valid');
 	
 	if (window.confirm('Your verdict has been recorded. OK to continue reviewing cases. Cancel to quit.')) {
-		resetPage();
+		loadCase();
 	} else {
 		$('body').html('<h1>Thank you for volunteering your time to enforce the summoner\'s code. This page will now close.</h1>');
 		window.close();
