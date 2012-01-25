f77e7b53bb6f079ea4b5a115d3e0e1321b3ab201
Noah Manneschmidt <noah@manneschmidt.net>
2012-01-24 21:14:44 -0600

    Fix a stray negative in the SSL check

--- support/partials.php
+++ support/partials.php
@@ -71,6 +71,6 @@ function usingSSL() {
 	return (
 		(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1))
 		||
-		(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] != 'https')
+		(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
 	);
 }
