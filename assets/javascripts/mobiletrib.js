aab963f23120cd66cfbbc0851a6425a850aafdf4
Noah Manneschmidt <noah@manneschmidt.net>
2012-05-31 19:35:24 -0500

    Update javascript for new tribunal JSON format, include teammates in layout

--- assets/javascripts/mobiletrib.js
+++ assets/javascripts/mobiletrib.js
@@ -35,7 +35,7 @@ $(function() {
 		$('#games').toggle();
 	});
 	// handle switching games
-	$('#games li').live('click', function() {
+	$(document).on('click', '#games li', function(event) {
 		$('#games').hide();
 		loadGame(this.value);
 	});
@@ -44,7 +44,7 @@ $(function() {
 	$('#return').click(function() {showOnly('game');});
 	
 	// handle showing inventory details
-	$('#inventory img').live('click', function() {
+	$(document).on('click', '#inventory img', function() {
 		var data = $(this).data('info');
 		// TODO replace this alert with a popover div that looks nice
 		alert(data.name + "\n" + data.description);
@@ -100,20 +100,26 @@ $(function() {
 			if (data.status === 'failed' || data.status === 'nosess') {
 				return showOnly('login');
 			}
-			if ( data.status === 'finished' )
+			if ( data.status === 'finished' ) {
 				return showOnly('finished');
-			if ( data.status === 'ok' )
-			{
+			}
+			if ( data.status === 'ok' ) {
 				loadCase(data);
 			}
 		}
-
 	});
 
 });
 
 function onUpdateReady() {
 	if (confirm('An update is available. Press OK to reload this page and apply the update.')) window.location.reload();
+}
+
+function urlPrefix() {
+	if (!window.urlPrefix) {
+		window.urlPrefix = 'https://' + $('#realm').val() + '.leagueoflegends.com';
+	}
+	return window.urlPrefix;
 }
 
 // shows only one component of the app and hides all the others
@@ -177,10 +183,9 @@ function submitLogin(event) {
 
 function processLoginResult(data) {
 	if (data.status === 'ok') {
-		//TODO hide login form, show the case review stuff and load the case
 		showOnly('game');
 		loadCase(data);
-	} else  {
+	} else {
 		// put each elemnt of response.feedback as a paragraph in #feedback
 		$('#feedback').html(data.feedback.join('<br>'));
 		showOnly('login');
@@ -196,6 +201,7 @@ function processCaseSubmissionResult(data) {
 	else if (data.status === 'finished')
 		showOnly('finished'); // TODO have a button to retry that checks if you are still expired
 	else if (data.status === 'nosess')
+		$('#feedback').html('Your session has expired.');
 		showOnly('login');
 	else if (data.status === 'ok')
 		loadCase(data);
@@ -227,7 +233,7 @@ function loadCase(data) {
 	
 	// handle verdict timer
 	$('#timer-message').show();
-	window.timeLeft = 60;
+	window.timeLeft = 20;
 	if (!window.timerInterval)
 		window.timerInterval = window.setInterval(timerTick, 1000);
 
@@ -240,14 +246,13 @@ function loadCase(data) {
 	// create the list of games
 	$('#games').empty();
 	for (var i=1; i<=num; i++) {
-		$('<li onclick="void(0)"></li>').attr('value',i).html('Game '+i).appendTo('#games');
+		$('<li onclick="void(0)"></li>').attr('value',i).html('<img src="assets/images/unknownplayer.png"> Game '+i).appendTo('#games');
 	}
 	
 	$('#caseid').html(data.case);
 	loadGame('1');
 	// if we are loading for the first time, grab a new captcha in the background
 	reloadCaptcha();
-
 }
 
 function timerTick() {
@@ -287,97 +292,108 @@ function loadGame(gameNumber) {
 
 // performs some parsing and caches the result of a fetched game
 function initData(gameData, gameNumber) {
-	// force secure links to images
-	var newItems = [];
-	gameData.champion = gameData.champion.replace(/^http:/,'https:');
-	for (var i=0; i<gameData.items.length; i++) {
-		if (gameData.items[i].icon) { // must check if the icon property exists, have seen some elements as simply {scalar: ""}
-			gameData.items[i].icon = gameData.items[i].icon.replace(/^http:/,'https:');
-			newItems.push(gameData.items[i]);
-		}
-	}
-	gameData.items = newItems;
-	// try to fix missing champ names
-	gameData.champsUsed = {};
-	for (var i=0; i<gameData.stats.length; i++) {
-		gameData.champsUsed[gameData.stats[i].NAME] = gameData.stats[i].SKIN;
-	}
-	// fix time string if it is over an hour
-	if (!$.isNumeric(gameData.stats[0].TIME_PLAYED)) {
-		var timechunks = /^(\d+)[\w\s]*?(\d+)$/.exec(gameData.stats[0].TIME_PLAYED);
-		gameData.stats[0].TIME_PLAYED = Number(timechunks[1]) * 60 + Number(timechunks[2]);
-	}
+	// calculate a regular timestamp
+	gameData.time_played = Math.floor(gameData.offender.time_played / 60) + ':' + (gameData.offender.time_played % 60)
 	// cache the fixed data
 	window.cachedGames[gameNumber] = gameData;
+	// apply champion portrait in games list
+	$('#games img')[gameNumber+1].src = urlPrefix()+gameData.offender.champion_url;
 	return gameData;
 }
 
 function applyData(gameData) {
 	// expand the data into the #game div
-	$('#summoner-name').text('"' + gameData.summoner + '"');
-	$('#portrait img').attr('src', gameData.champion);
-	$('#portrait img').attr('alt', gameData.champion_name || gameData.champsUsed[gameData.summoner]);
-	$('#champname span').text(gameData.champion_name || gameData.champsUsed[gameData.summoner]);
+	$('#summoner-name').text('"' + gameData.offender.summoner_name + '"');
+	$('#portrait img').attr('src', urlPrefix()+gameData.offender.champion_url);
+	$('#portrait img').attr('alt', gameData.offender.champion_name);
+	$('#champname span').text(gameData.offender.champion_name);
+	$('#summoner1').attr('src', urlPrefix()+gameData.offender.summoner_spell_1);
+	$('#summoner2').attr('src', urlPrefix()+gameData.offender.summoner_spell_2);
 	
-	var stats = gameData.stats[0];
-	$('#level').text(stats.LEVEL);
-	$('#time').text(stats.TIME_PLAYED + ':00'); //fudge the time because riot only gives minutes
-	$('#kills').text(stats.SCORES[0]);
-	$('#deaths').text(stats.SCORES[1]);
-	$('#assists').text(stats.SCORES[2]);
-	$('#outcome').text(stats.WIN);
-	$('#minion-kills').text(stats.MINIONS_KILLED);
-	$('#dps-out').text(stats.TOTAL_DAMAGE_DEALT);
-	$('#dps-in').text(stats.TOTAL_DAMAGE_RECEIVED);
+	$('#level').text(gameData.offender.level);
+	$('#time').text(gameData.time_played);
+	// we should probably display different stats if dominion
+	$('#kills').text(gameData.offender.scores.kills);
+	$('#deaths').text(gameData.offender.scores.deaths);
+	$('#assists').text(gameData.offender.scores.assists);
+	$('#outcome').text(stats.offender.outcome);
+	$('#minion-kills').text(stats.offender.minions_killed);
+	$('#gold-earned').text(stats.offender.gold_earned);
+	$('#date-played').text(stats.game_creation_time);
+	$('#dps-out').text(stats.offender.total_damage_dealt);
+	$('#dps-in').text(stats.offender.total_damage_received);
 	
 	// setup inventory-container
 	$('#inventory-container').empty();
-	for (var i=0; i<gameData.items.length; i++) {
-		var item = gameData.items[i];
-		$('<img>')
-			.attr('src', item.icon)
-			.attr('title', item.name)
-			.attr('alt', item.name)
-			.data('info', item)
-			.appendTo('#inventory-container');
+	for (var i=0; i<gameData.offender.items.length; i++) {
+		var item = gameData.offender.items[i];
+		if (item.name !== '')
+			$('<img>')
+				.attr('src', urlPrefix()+item.icon)
+				.attr('title', item.name)
+				.attr('alt', item.name)
+				.data('info', item)
+				.appendTo('#inventory-container');
 	}
-	
+
+	//display teammates
+	$('#teammates tbody').empty();
+	for (var playerid in gameData.players) {
+		var player = gameData.players[playerid];
+		if (player.association_to_offender === 'ally') {
+			var teammate = $('#teammates thead tr.template').clone();
+			teammate.removeClass('template').addClass('teammate');
+			teammate.find('.port img').attr('src', urlPrefix()+player.champion_url);
+			teammate.find('.level').html(player.level);
+			teammate.find('.champname').html(player.champion_name);
+			teammate.find('.score').html(player.scores.kills +'/'+ player.scores.deaths +'/'+ player.scores.assists);
+			teammate.find('.summ1').attr('src', urlPrefix()+player.summoner_spell_1);
+			teammate.find('.summ2').attr('src', urlPrefix()+player.summoner_spell_2);
+			teammate.find('.gold').html(player.gold_earned);
+			teammate.find('.cs').html(player.minions_killed);
+			for (var i = player.items.length - 1; i >= 0; i--) {
+				var item = teammate.find('.item'+i);
+				if (player.items[i].name !== '') {
+					item.attr('src', urlPrefix()+player.items[i].icon);
+				} else {
+					item.attr('src', 'assets/images/itemslot.png');
+				}
+			}
+			teammate.appendTo('#teammates tbody');
+		}
+	}
+
 	// add reports
 	$('#reports').empty();
 	$('#allied-report-count').html(gameData.allied_report_count);
-	var newDiv = $("<div/>");
-	for (var i=0; i<gameData.allied_report_count; i++) {
-		var report = newDiv.html(gameData.allycomments[i]).text();
-		var reason = gameData.allyreportreasons[i];
-		$('<li class="allied"></li>').html(report).append($('<h2></h2>').html(reason)).appendTo('#reports');
-	}
 	$('#enemy-report-count').html(gameData.enemy_report_count);
-	for (var i=0; i<gameData.enemy_report_count; i++) {
-		var report = gameData.enemycomments[i];
-		var reason = gameData.enemyreportreasons[i];
-		$('<li class="enemy"></li>').html(report).append($('<h2></h2>').html(reason)).appendTo('#reports');
+	var newDiv = $("<div/>"), reports = {enemy:$(),ally:$()};
+	for (var i = gameData.reports.length - 1; i >= 0; i--) {
+		var report = gameData.reports[i], item = $('<li></li>');
+		item.addClass(report.association_to_offender);
+		item.html(report.comment).append($('<h2></h2>').html(report.offense));
+		reports[report.association_to_offender].add(item);
 	}
+	reports.ally.appendTo('#reports');
+	reports.enemy.appendTo('#reports');
 	
 	// build chat log
 	var $chat = $('#chat').empty();
 	var chatLength = gameData.chatlogtext.length;
-	for (var i=0; i<chatLength; i++) {
-		var summoner = gameData.chatlogusers[i];
-		var champion = gameData.chatlogchampions[i] || gameData.champsUsed[summoner];
-		var timestamp = gameData.chatloggametime[i];
-		var message = gameData.chatlogtext[i];
-		var classes = gameData.chatlogteams[i];
-		if (classes === 'reported') classes += ' allied';
-		if (gameData.chatlogsentto[i] === 'All') classes += ' all';
+	for (var i = gameData.chat_log.length - 1; i >= 0; i--) {
+		var chat_line = gameData.chat_log[i];
+		var classes = chat_line.association_to_offender;
+		if (classes === 'offender') classes += ' ally';
+		if (chat_line.sent_to === 'All') classes += ' all';
 		$('<li></li>').addClass(classes)
 			.append(
 				$('<span class="author"></span>')
-					.append($('<span class="summoner"></span>').text(summoner))
-					.append($('<span class="character"></span>').text(champion))
+					.append($('<span class="summoner"></span>').text(chat_line.summoner_name))
+					.append($('<span class="character"></span>').text(chat_line.champion_name))
 			)
-			.append($('<time></time>').text(timestamp))
-			.append(': '+message)
-			.appendTo($chat);
+			.append($('<time></time>').text(chat_line.time))
+			.append(': '+chat_line.message)
+			.prependTo($chat);
 	}
 	
 	// reset chat filter controls
@@ -388,7 +404,6 @@ function applyData(gameData) {
 	showOnly('game');
 }
 
-function disclaim()
-{
+function disclaim() {
 	alert("Mobile Tribunal is provided as is with no guarantees as to its functionality. While every effort is made to protect your information (all connections are encrypted with SSL), the Mobile Tribunal is not responsible for theft of your data, including, but not limited to, usernames and passwords.");
 }
