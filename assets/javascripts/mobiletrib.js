62276d23596d1824f502a911a0b6fc2d07a8e969
unknown <Aram@aramdesk.(none)>
2012-09-16 10:55:27 -0700

    updated image urls

--- assets/javascripts/mobiletrib.js
+++ assets/javascripts/mobiletrib.js
@@ -115,7 +115,7 @@ function urlPrefix() {
 }
 
 function formatImageUrl(url) {
-	return url.replace('/tribunal/bundles/riothelper', urlPrefix()+'/sites/default/files');
+	return urlPrefix()+url;
 }
 
 // shows only one component of the app and hides all the others
